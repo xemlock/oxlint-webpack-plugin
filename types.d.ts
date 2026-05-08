3f277b4df1340b04246231d267a823881d7407a0
xemlock <952555+xemlock@users.noreply.github.com>
2026-05-08 20:22:06 +0200

    Provide proper type definitions

--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,11 @@
-declare module "oxlint-webpack-plugin";
+import { Compiler, WebpackPluginInstance } from "webpack";
+
+export interface Options {
+  format?: "default" | "stylish";
+  childProcessMaxFiles?: number;
+}
+
+export class OxLintWebpackPlugin implements WebpackPluginInstance {
+  constructor(options?: Options);
+  apply: (compiler: Compiler) => void;
+}
